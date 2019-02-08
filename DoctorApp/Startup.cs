7b6748d694c4261119636acd2995b2959e796555
luisjjavier <javier.luis29@gmail.com>
2019-02-07 22:07:19 -0400

    Add project files.

---
+++ DoctorApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoctorApp.Startup))]
+namespace DoctorApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
